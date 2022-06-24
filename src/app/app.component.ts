9e093d74139498c41286a387bf1f2d9c05eaf05b
Bruno Araujo <bruno.aoliveira@al.infnet.edu.br>
2022-06-24 15:40:52 -0300

    commit do header e da primeira parte da home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ecommerce';
+  
+  position: number = 0;
+
+  getPosition(){
+    this.position = window.pageYOffset;  
+  }
 }
